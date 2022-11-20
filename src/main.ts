cb9eff0264d72648883013d635f5d1191882f7e7
Noriuki <lanezupiroli@gmail.com>
2022-11-20 01:29:19 -0300

    add domain to cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.PROD_URL || 'http://localhost:3000',
+    origin: process.env.PROD_URL || 'https://sigvet-app.com.br',
   });
 
   app.setGlobalPrefix('/api');
