1c3707d9f0eb20df170b46b0cf6461950a6209de
Noriuki <lanezupiroli@gmail.com>
2022-11-20 09:44:50 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: process.env.PROD_URL || 'https://sigvet-app.com.br',
-  });
+  app.enableCors();
 
   app.setGlobalPrefix('/api');
   await app.listen(process.env.PORT || 3001);
